76e24f2a84205bf0ec116a07fd025fe5102b4a05
Marek Kaszycki <marek.kaszycki@northmill.se>
2022-07-27 13:00:05 +0200

    Use DiffObservableList. Update item view model

--- app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/NotesListViewModel.kt
+++ app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/NotesListViewModel.kt
@@ -1,8 +1,8 @@
 package com.mksoftware101.personalnotes.ui.noteslist
 
-import androidx.databinding.ObservableArrayList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.recyclerview.widget.DiffUtil
 import com.mksoftware101.personalnotes.BR
 import com.mksoftware101.personalnotes.R
 import com.mksoftware101.personalnotes.domain.GetAllNotesUseCase
@@ -11,6 +11,7 @@ import com.mksoftware101.personalnotes.ui.noteslist.item.OnNotesListItemClickLis
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import me.tatarka.bindingcollectionadapter2.ItemBinding
+import me.tatarka.bindingcollectionadapter2.collections.DiffObservableList
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -22,19 +23,33 @@ class NotesListViewModel
         Timber.d("[d] clicked id=${item.title}")
     }
 
-    val items = ObservableArrayList<NotesListItemViewModel>()
+
+    val items = DiffObservableList(object : DiffUtil.ItemCallback<NotesListItemViewModel>() {
+        override fun areItemsTheSame(
+            oldItem: NotesListItemViewModel,
+            newItem: NotesListItemViewModel
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: NotesListItemViewModel,
+            newItem: NotesListItemViewModel
+        ): Boolean {
+            return oldItem.title == newItem.title
+        }
+
+    })
     val itemBinding =
         ItemBinding.of<NotesListItemViewModel>(BR.viewModel, R.layout.view_noteslist_item)
             .bindExtra(BR.listener, listener)
 
     fun getAllNotes() {
         viewModelScope.launch {
-            getAllNotesUseCase.run().collect {
-                Timber.d("[d] size=${it.size}, notes=$it")
-                it.map {
-                    val item = NotesListItemViewModel(it.title)
-                    items.add(item)
-                }
+            getAllNotesUseCase.run().collect { notesList ->
+                Timber.d("[d] size=${notesList.size}, notes=$notesList")
+                val newItems = notesList.map { NotesListItemViewModel(it.Id, it.title) }
+                items.update(newItems)
             }
         }
     }
--- app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/item/NotesListItemViewModel.kt
+++ app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/item/NotesListItemViewModel.kt
@@ -2,4 +2,4 @@ package com.mksoftware101.personalnotes.ui.noteslist.item
 
 import androidx.lifecycle.ViewModel
 
-class NotesListItemViewModel(val title: String) : ViewModel()
+class NotesListItemViewModel(val id: Long, val title: String) : ViewModel()
