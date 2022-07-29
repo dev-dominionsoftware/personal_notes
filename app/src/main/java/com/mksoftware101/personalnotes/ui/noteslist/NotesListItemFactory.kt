d1a8fbb6dcb160df7832af9a4c9da5768434e0f8
Marek Kaszycki <marek.kaszycki@northmill.se>
2022-07-29 15:47:41 +0200

    Date section in notes list created

--- app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/NotesListItemFactory.kt
+++ app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/NotesListItemFactory.kt
@@ -1,10 +1,15 @@
 package com.mksoftware101.personalnotes.ui.noteslist
 
 import com.mksoftware101.personalnotes.domain.model.Note
+import com.mksoftware101.personalnotes.ui.noteslist.item.NotesListItemDateViewModel
 import com.mksoftware101.personalnotes.ui.noteslist.item.NotesListItemViewModel
+import com.mksoftware101.personalnotes.ui.noteslist.item.base.NotesListItemBaseViewModel
 
 class NotesListItemFactory {
-    fun assemble(notesList: List<Note>): List<NotesListItemViewModel> {
-        return notesList.map { NotesListItemViewModel(it.Id, it.title) }
+    fun assemble(notesList: List<Note>): List<NotesListItemBaseViewModel> {
+        val itemsList = mutableListOf<NotesListItemBaseViewModel>()
+        itemsList.add(NotesListItemDateViewModel("10 July 2022"))
+        notesList.map { itemsList.add(NotesListItemViewModel(it.Id, it.title)) }
+        return itemsList
     }
 }
