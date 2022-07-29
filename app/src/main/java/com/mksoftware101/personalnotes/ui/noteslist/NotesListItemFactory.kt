6a5699ac9d4c9487b866f7947c16c51bd1c0cff3
Marek Kaszycki <marek.kaszycki@northmill.se>
2022-07-29 16:42:52 +0200

    Use LocalDate for date section

--- app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/NotesListItemFactory.kt
+++ app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/NotesListItemFactory.kt
@@ -6,6 +6,7 @@ import com.mksoftware101.personalnotes.domain.model.Note
 import com.mksoftware101.personalnotes.ui.noteslist.item.NotesListItemDateViewModel
 import com.mksoftware101.personalnotes.ui.noteslist.item.NotesListItemViewModel
 import com.mksoftware101.personalnotes.ui.noteslist.item.base.NotesListItemBaseViewModel
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -18,13 +19,18 @@ class NotesListItemFactory {
         val itemsList = mutableListOf<NotesListItemBaseViewModel>()
 
         val onlyDates = notesList.map {
-            it.creationDate
+            val dateTime = it.creationDate
+            LocalDate.of(dateTime.year, dateTime.month, dateTime.dayOfMonth)
         }
+
         val noDuplicates = onlyDates.distinct()
 
         val alltogether = noDuplicates.map { date ->
-            val associatedNotes = notesList.filter { it.creationDate == date }
-            Section(date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), associatedNotes)
+            val associatedNotes = notesList.filter {
+                val cd = LocalDate.of(it.creationDate.year, it.creationDate.month, it.creationDate.dayOfMonth)
+                cd == date
+            }
+            Section(date.format(DateTimeFormatter.ISO_LOCAL_DATE), associatedNotes)
         }
 
         alltogether.map { section ->
