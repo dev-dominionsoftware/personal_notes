659524025c480c33f8e60f1a48f6b31485bf1406
Marek Kaszycki <marek.kaszycki@northmill.se>
2022-08-03 21:47:53 +0200

    Implement LetterInCircle in item layout

--- app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/item/NotesListItemViewModel.kt
+++ app/src/main/java/com/mksoftware101/personalnotes/ui/noteslist/item/NotesListItemViewModel.kt
@@ -2,4 +2,7 @@ package com.mksoftware101.personalnotes.ui.noteslist.item
 
 import com.mksoftware101.personalnotes.ui.noteslist.item.base.NotesListItemBaseViewModel
 
-class NotesListItemViewModel(val id: Long, val title: String) : NotesListItemBaseViewModel()
+class NotesListItemViewModel(val id: Long, val title: String) : NotesListItemBaseViewModel() {
+    val letter: String
+        get() = title.take(1)
+}
